8fdae517924b14b8ea880c00d97bf163c19ea53d
Cheuk Ki Yu <141883247+yuzhuoqi@users.noreply.github.com>
2026-05-15 22:55:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="LookFy", layout="centered")
+st.set_page_config(page_title="LookiFy", layout="centered")
 
 # ===== ESTILO =====
 st.markdown(
