4432d4ca2f993edaa80083c3bd9526f0f2e83c49
Cheuk Ki Yu <141883247+yuzhuoqi@users.noreply.github.com>
2026-05-17 23:04:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -282,7 +282,7 @@ with c1:
         "https://images.unsplash.com/photo-1541099649105-f69ad21f3246",
         use_container_width=True
     )
-    st.markdown("**Trench coat bege**")
+    st.markdown('<div class="product-title">Aura Jacket</div>', unsafe_allow_html=True)
     st.markdown('<div class="product-price">R$ 129,90</div>', unsafe_allow_html=True)
     st.markdown('</div>', unsafe_allow_html=True)
 
@@ -292,7 +292,7 @@ with c2:
         "https://images.unsplash.com/photo-1542291026-7eec264c27ff",
         use_container_width=True
     )
-    st.markdown("**Bota de couro**")
+    st.markdown('<div class="product-title">Nebula Sneakers</div>', unsafe_allow_html=True)
     st.markdown('<div class="product-price">R$ 179,90</div>', unsafe_allow_html=True)
     st.markdown('</div>', unsafe_allow_html=True)
 
@@ -302,7 +302,7 @@ with c3:
         "https://images.unsplash.com/photo-1584917865442-de89df76afd3",
         use_container_width=True
     )
-    st.markdown("**Bolsa marrom**")
+    st.markdown('<div class="product-title">Luna Bag</div>', unsafe_allow_html=True)
     st.markdown('<div class="product-price">R$ 89,90</div>', unsafe_allow_html=True)
     st.markdown('</div>', unsafe_allow_html=True)
 
